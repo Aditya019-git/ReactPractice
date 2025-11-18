ea1458a70814a9d416438c8f265b9aab4e140a1a
Aditya019 <adityapande019@gmail.com>
2025-11-18 13:08:42 +0530

    Hooks Understanding

--- 01Basic/src/App.jsx
+++ 01Basic/src/App.jsx
@@ -4,12 +4,13 @@
 
 import Chai from "./Chai"
 function App() {
+  const name="Aditya Satish Pande"
 
 
   return (
     <>
       <Chai/>   
-      <h1>Chai Aur React with Vite || Aditya Pande</h1>
+      <h1>Chai Aur React with Vite || {name}</h1>
     </>
 
   )
