fe799fb5dd55d80e04072ce05abda7582fbd5987
Aditya019 <adityapande019@gmail.com>
2025-11-19 13:22:32 +0530

    BgChnager Project

---
+++ TailwindProps/src/App.jsx
@@ -0,0 +1,17 @@
+import Card from "./Components/Card"
+
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>My Friends Cards </h1>
+     <Card username="Gopal" btnText="Click me"/>
+     <Card username="Omkar" btnText="visit me"/>
+     <Card username="Mohit" btnText="fuck  me"/>
+    </>
+  )
+}
+
+export default App
